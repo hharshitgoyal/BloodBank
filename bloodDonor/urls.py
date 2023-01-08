b923471c6f2efd02ece74660006c3e3660041562
harshitgoyal <harshit.goyal@nagarro.com>
2023-01-09 02:19:32 +0530

    Url fixed

--- bloodDonor/urls.py
+++ bloodDonor/urls.py
@@ -3,5 +3,5 @@ from bloodDonor import views
 
 urlpatterns = [
     url('donorDetails', views.BloodDonorView.as_view()),
-    url('detailsById/(?P<pk>[0-9]+)$', views.BloodDonorViewById.as_view()),
+    url('donorDetailsById/(?P<pk>[0-9]+)$', views.BloodDonorViewById.as_view()),
 ]
